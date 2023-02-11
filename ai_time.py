8693595252f7f9fb1ac1c720b823fc3b15747b63
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-02-11 15:33:09 -0800

    Update ai_time.py

--- ai_time.py
+++ ai_time.py
@@ -1,42 +1,45 @@
 import datetime  
 
 while (True):
-  ## get the name of the city from the user
   city = input("Enter city: ")
 
-  ## get the current time 
   current_time = datetime.datetime.now()
-
-  ## save hours, minutes and second of the current time 
-  ## in their corresponding variables 
   hour = current_time.hour
   minute = current_time.minute
   second = current_time.second
   
-  ## check for case 1: the learner has typed Boston
   if city == "Boston":
     hour = hour - 4 
 
-  ## check for case 2: the learner has typed Tokyo
   elif city == "Tokyo":
     hour = hour + 9
 
-  ## check for case 3: the learner has typed Chicago
   elif city == "Chicago":
     hour = hour - 5 
 
-  ## check for case 4: the learner has typed Seattle
   elif city == "Seattle":
     hour = hour - 7
+
+  ## add more cities here
   
-  ## check for case 5: the learner has typed exit
+  if city == "Kolkata":
+    hour = hour + 5
+    minutes = minutes + 30
+
+  if city == "Karachi":
+    hour = hour + 5
+
+  if city == "Cairo":
+    hour = hour + 2
+  
+  if city == "Shanghai":
+    hour = hour + 8
+
   elif city == "exit": 
     break
-
-  ## if all cases fail, show the time for GMT    
+    
   else:
     print(city, "is not added")
     city = "GMT" 
 
-  # print the name of the city and the its corresponding time
   print(city, str(hour) + ":" + str(minute) + ":" + str(second))
